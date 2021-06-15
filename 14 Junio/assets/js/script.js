f7d5adea388f6b6f4c7df9cfb16e6d1dae48cfc0
ClaraItzel <claraitzelle@gmail.com>
2021-06-15 16:59:35 -0500

    DOM js

---
+++ 14 Junio/assets/js/script.js
@@ -0,0 +1,8 @@
+/* let parrafos = document.getElementsByClassName('parrafo');
+console.log(parrafos);
+
+document.getElementsByClassName('parrafo')[0].innerHTML='Ola ka ase?' */
+document.querySelector('.parrafo').textContent='Solo quedaaaaan'
+
+console.log(document.head);
+console.log(document.body);
---
+++ 15 Junio/assets/scripts.js
@@ -0,0 +1,47 @@
+/* let dato1=24;
+let dato2="Holaaaa";
+let dato3="false";
+let dato4="5.37";
+let dato5=true;
+let dato6=null;
+let dato7="Hoy es un lindo dia";
+let dato8=829;
+let dato9="null";
+let dato10=false;
+
+console.log(typeof(dato1));
+console.log(typeof(dato2));
+console.log(typeof(dato3));
+console.log(typeof(dato4));
+console.log(typeof(dato5));
+console.log(typeof(dato6));
+console.log(typeof(dato7));
+console.log(typeof(dato8));
+console.log(typeof(dato9));
+console.log(typeof(dato10));
+ */
+let nueva_variable = 'Mi nueva variable';
+console.log(nueva_variable);
+function letTest(){
+    let x=31;
+    if(true){
+        let x=71;
+        console.log(x);
+    }
+    console.log(x);
+}
+letTest();
+const nuevaFuncion = () =>{
+    var x=31;
+    if(true){
+        var x= 71;
+        console.log(x);
+    }
+    console.log(x);
+}
+const nombre= 'Clara';
+const apellidos= 'Gómez Arriaga'
+console.log(`Hola, mi nombre es ${nombre} mis apellidos son ${apellidos}`);
+
+const arreglo=[1,2,3,4,5];
+console.log(arreglo);
